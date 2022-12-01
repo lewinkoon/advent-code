5b00f8111b0b8f3b8ae6b49f3e3541b4b0530665
Lewin <lewinkoon+github@gmail.com>
2022-12-01 20:40:10 +0100

    fix format

--- 2022/day-01/src/main.rs
+++ 2022/day-01/src/main.rs
@@ -17,12 +17,11 @@ fn main() {
     list.sort();
     list.reverse();
 
-
     // answers
     let ans_one: i32 = list[0];
     let ans_two: i32 = list[..3].iter().sum();
 
     println!("\nAnswers:\n");
-    println!("a) {:?} calories\n", ans_one);
-    println!("b) {:?} calories\n", ans_two);
+    println!("a) {} calories\n", ans_one);
+    println!("b) {} calories\n", ans_two);
 }
