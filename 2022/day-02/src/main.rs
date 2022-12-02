5ed5e9550d124d7118255abffbaaa49087256643
Lewin <lewinkoon+github@gmail.com>
2022-12-02 21:46:44 +0100

    fix 2nd day

--- 2022/day-02/src/main.rs
+++ 2022/day-02/src/main.rs
@@ -6,19 +6,17 @@ fn main() {
     let mut score_a = 0;
     let mut score_b = 0;
     for round in input.split("\n") {
-        for shape in round.split("\n") {
-            match shape {
-                "B X" => {score_a += 1; score_b += 1},
-                "C Y" => {score_a += 2; score_b += 6},
-                "A Z" => {score_a += 3; score_b += 8},
-                "A X" => {score_a += 4; score_b += 3},
-                "B Y" => {score_a += 5; score_b += 5},
-                "C Z" => {score_a += 6; score_b += 7},
-                "C X" => {score_a += 7; score_b += 2},
-                "A Y" => {score_a += 8; score_b += 4},
-                "B Z" => {score_a += 9; score_b += 9},
-                _ => println!("Error")
-            }
+        match round {
+            "B X" => {score_a += 1; score_b += 1},
+            "C Y" => {score_a += 2; score_b += 6},
+            "A Z" => {score_a += 3; score_b += 8},
+            "A X" => {score_a += 4; score_b += 3},
+            "B Y" => {score_a += 5; score_b += 5},
+            "C Z" => {score_a += 6; score_b += 7},
+            "C X" => {score_a += 7; score_b += 2},
+            "A Y" => {score_a += 8; score_b += 4},
+            "B Z" => {score_a += 9; score_b += 9},
+            _ => println!("Error")
         }
     }
 
