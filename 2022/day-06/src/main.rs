f7610717ad17e7adb84a7f31339ffec0987f50c8
Lewin <lewinkoon+github@gmail.com>
2022-12-06 11:18:00 +0100

    add 6th day

---
+++ 2022/day-06/src/main.rs
@@ -0,0 +1,19 @@
+use itertools::Itertools;
+use std::fs;
+
+const BUFF: usize = 14;
+
+fn main() {
+    let input: String = fs::read_to_string("./test.txt").expect("File could not be read");
+
+    let marker = input
+        .chars()
+        .collect_vec()
+        .windows(BUFF)
+        .enumerate()
+        .filter(|(_, w)| w.into_iter().all_unique())
+        .map(|(i, _)| i + BUFF )
+        .next().unwrap();
+
+    println!("{}", marker);
+}
